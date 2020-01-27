22ea341a351000d43c2f30fc9b2e2b37425ffb06
HZJ <1054919923@qq.com>
2020-01-27 18:20:06 +0800

    update all

---
+++ README.md
@@ -0,0 +1,3 @@
+zsimple01@163.com
+zsimple02@163.com
+zsimple03@163.com
--- web/homepage/tool.php
+++ web/homepage/tool.php
@@ -1,18 +1,26 @@
 <?php
 
+for ($i = 0; $i < 1000; $i++) {
+    $randNumber = mt_rand(0, 5);
+    $class = ["", "", "","", "too-big-letter-grid", "big-letter-grid"][$randNumber];
+    // echo $class;
+
+    echo "<div class=\"letter $class\">$i</div>";
+}
+die();
 $i = 1;
-while($i--){
-    echo 
-    '0% {
+while ($i--) {
+    echo
+        '0% {
         text-shadow: 0 0 30px #fff
     }';
-    for($num=1;$num<100;$num++)
-    echo '
+    for ($num = 1; $num < 1000; $num++)
+        echo '
 
-    '.$num.'% {
-        text-shadow: 0 0 30px rgb('.mt_rand(0,255).', '.mt_rand(0,255).', '.mt_rand(0,255).')
+    ' . $num . '% {
+        text-shadow: 0 0 30px rgb(' . mt_rand(0, 255) . ', ' . mt_rand(0, 255) . ', ' . mt_rand(0, 255) . ')
     }';
-echo '
+    echo '
     100% {
         text-shadow: 0 0 30px #fff
     }';
